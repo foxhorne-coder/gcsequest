abd700a89517eca15e84e47a6f6c4c658f310f1b
foxhorne-coder <fox.horne@icloud.com>
2026-09-03 18:13:00 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithRedirect, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
-import { getFirestore, doc, setDoc, collection, getDocs, query, orderBy, limit } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
-import { firebaseConfig } from "./firebase-config.js";
-const app=initializeApp(firebaseConfig); export const auth=getAuth(app); export const db=getFirestore(app);
-export const loginWithGoogle=()=>signInWithRedirect(auth,new GoogleAuthProvider());
-export const logout=()=>signOut(auth); export const watchAuth=cb=>onAuthStateChanged(auth,cb);
-export async function saveScore(user,xp){if(user)await setDoc(doc(db,"users",user.uid),{displayName:user.displayName||"Student",xp:Number(xp)||0,updatedAt:Date.now()},{merge:true});}
-export async function getLeaderboard(){const s=await getDocs(query(collection(db,"users"),orderBy("xp","desc"),limit(50)));return s.docs.map(d=>d.data());}
+import { signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+export async function loginWithGoogle(auth) {
+  const provider = new GoogleAuthProvider();
+  return await signInWithPopup(auth, provider);
+}
